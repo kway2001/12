cf093bb5c668d606c4d14b52e9cdabffd552ffd5
kway2001 <70741593+kway2001@users.noreply.github.com>
2020-11-19 15:57:40 +0900

    practice 5

--- main.c
+++ main.c
@@ -5,18 +5,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	//실습 3 
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
 	
-	strcpy(dst, src);
+	char str[100];
+	int i;
+	FILE *fp;
+	fp = fopen("sample.txt", "w");
+	if (fp == NULL)
+	{
+		return -1;
+	}
 	
-	printf("copide string: %s\n", dst);
-	
-	//실습 4 
-	char str[30] = "happy C programming";
-	
-	printf("length of \"%s\" : %i\n", str, strlen(str));
-	
+	for (i=0; i<3; i++) {
+	printf("input a word: ");
+	scanf("%s", str);
+	fprintf(fp, "%s\n", str);
+	}
+	fclose(fp);
 	return 0;
 }
